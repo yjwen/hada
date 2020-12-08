da9cb5ec59f4ceb581833d0272e04d152e127412
Yujie Wen <yjwen.ty@qq.com>
2020-12-08 23:43:26 +0800

    Fix some typo in README

--- examples/TestPlus.hs
+++ examples/TestPlus.hs
@@ -2,7 +2,7 @@ import System.Exit
 import Plus
 import qualified Wplus
 
-main = do test <-  12 `Wplus.plus` 34
+main = do test <- 12 `Wplus.plus` 34
           let golden = 12 `plus` 34
           if test == golden
             then exitSuccess
