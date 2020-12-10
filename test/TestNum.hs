28cad38fe66cd9ba1b3db4b1cef6e57b129a3754
Yujie Wen <yjwen.ty@qq.com>
2020-12-10 23:17:18 +0800

    Rewrite TestNum by more compact code

--- test/TestNum.hs
+++ test/TestNum.hs
@@ -42,37 +42,38 @@ repeatedTest test = TestCase ((sequence $ replicate 1024 test) >>= assertAllNoth
         assertAllNothing (x:xs) | Just msg <- x = assertFailure msg
                                 | Nothing  <- x = assertAllNothing xs
 
-numTests = TestList [ TestLabel "plus" $ repeatedTest $ testNum plus Wplus.plus
-                    , TestLabel "plus8" $ repeatedTest $ testNum plus8 Wplus8.plus8
-                    , TestLabel "plus16" $ repeatedTest $ testNum plus16 Wplus16.plus16
-                    , TestLabel "plus32" $ repeatedTest $ testNum plus32 Wplus32.plus32
-                    , TestLabel "plus64" $ repeatedTest $ testNum plus64 Wplus64.plus64
-                    , TestLabel "plusW" $ repeatedTest $ testNum plusW WplusW.plusW
-                    , TestLabel "plusW8" $ repeatedTest $ testNum plusW8 WplusW8.plusW8
-                    , TestLabel "plusW16" $ repeatedTest $ testNum plusW16 WplusW16.plusW16
-                    , TestLabel "plusW32" $ repeatedTest $ testNum plusW32 WplusW32.plusW32
-                    , TestLabel "plusW64" $ repeatedTest $ testNum plusW64 WplusW64.plusW64
-                    , TestLabel "minus" $ repeatedTest $ testNum minus Wminus.minus
-                    , TestLabel "minus8" $ repeatedTest $ testNum minus8 Wminus8.minus8
-                    , TestLabel "minus16" $ repeatedTest $ testNum minus16 Wminus16.minus16
-                    , TestLabel "minus32" $ repeatedTest $ testNum minus32 Wminus32.minus32
-                    , TestLabel "minus64" $ repeatedTest $ testNum minus64 Wminus64.minus64
-                    , TestLabel "minusW" $ repeatedTest $ testNum minusW WminusW.minusW
-                    , TestLabel "minusW8" $ repeatedTest $ testNum minusW8 WminusW8.minusW8
-                    , TestLabel "minusW16" $ repeatedTest $ testNum minusW16 WminusW16.minusW16
-                    , TestLabel "minusW32" $ repeatedTest $ testNum minusW32 WminusW32.minusW32
-                    , TestLabel "minusW64" $ repeatedTest $ testNum minusW64 WminusW64.minusW64
-                    , TestLabel "mul" $ repeatedTest $ testNum mul Wmul.mul
-                    , TestLabel "mul8" $ repeatedTest $ testNum mul8 Wmul8.mul8
-                    , TestLabel "mul16" $ repeatedTest $ testNum mul16 Wmul16.mul16
-                    , TestLabel "mul32" $ repeatedTest $ testNum mul32 Wmul32.mul32
-                    , TestLabel "mul64" $ repeatedTest $ testNum mul64 Wmul64.mul64
-                    , TestLabel "mulW" $ repeatedTest $ testNum mul Wmul.mul
-                    , TestLabel "mulW8" $ repeatedTest $ testNum mulW8 WmulW8.mulW8
-                    , TestLabel "mulW16" $ repeatedTest $ testNum mulW16 WmulW16.mulW16
-                    , TestLabel "mulW32" $ repeatedTest $ testNum mulW32 WmulW32.mulW32
-                    , TestLabel "mulW64" $ repeatedTest $ testNum mulW64 WmulW64.mulW64
-                    ]
+numTests = (TestList . map (\(name, test) -> TestLabel name $ repeatedTest test))
+           [ ("plus" , testNum plus Wplus.plus)
+           , ("plus8" , testNum plus8 Wplus8.plus8)
+           , ("plus16" , testNum plus16 Wplus16.plus16)
+           , ("plus32" , testNum plus32 Wplus32.plus32)
+           , ("plus64" , testNum plus64 Wplus64.plus64)
+           , ("plusW" , testNum plusW WplusW.plusW)
+           , ("plusW8" , testNum plusW8 WplusW8.plusW8)
+           , ("plusW16" , testNum plusW16 WplusW16.plusW16)
+           , ("plusW32" , testNum plusW32 WplusW32.plusW32)
+           , ("plusW64" , testNum plusW64 WplusW64.plusW64)
+           , ("minus" , testNum minus Wminus.minus)
+           , ("minus8" , testNum minus8 Wminus8.minus8)
+           , ("minus16" , testNum minus16 Wminus16.minus16)
+           , ("minus32" , testNum minus32 Wminus32.minus32)
+           , ("minus64" , testNum minus64 Wminus64.minus64)
+           , ("minusW" , testNum minusW WminusW.minusW)
+           , ("minusW8" , testNum minusW8 WminusW8.minusW8)
+           , ("minusW16" , testNum minusW16 WminusW16.minusW16)
+           , ("minusW32" , testNum minusW32 WminusW32.minusW32)
+           , ("minusW64" , testNum minusW64 WminusW64.minusW64)
+           , ("mul" , testNum mul Wmul.mul)
+           , ("mul8" , testNum mul8 Wmul8.mul8)
+           , ("mul16" , testNum mul16 Wmul16.mul16)
+           , ("mul32" , testNum mul32 Wmul32.mul32)
+           , ("mul64" , testNum mul64 Wmul64.mul64)
+           , ("mulW" , testNum mul Wmul.mul)
+           , ("mulW8" , testNum mulW8 WmulW8.mulW8)
+           , ("mulW16" , testNum mulW16 WmulW16.mulW16)
+           , ("mulW32" , testNum mulW32 WmulW32.mulW32)
+           , ("mulW64" , testNum mulW64 WmulW64.mulW64)
+           ]
 
 -- | Compare verilated binary function with its origin, return Nothing
 -- at success, or a description at a failure
