d458e19ceeb1742161eec8d8a4c9d6d4dc0aacd8
Yujie Wen <yjwen.ty@qq.com>
2021-04-30 22:34:30 +0800

    Topo-last node related functions and topoFoldr

--- src/TopoGraph.hs
+++ src/TopoGraph.hs
@@ -1,29 +1,46 @@
-module TopoGraph (isTopoFree, findTopoFirst, topoFoldl) where
+module TopoGraph ( isTopoFirst, isTopoLast
+                 , findTopoFirst, findTopoLast
+                 , topoFoldl, topoFoldr) where
 
 import Algebra.Graph (Graph(..), hasVertex, removeVertex)
 
--- | isTopoFree x g == true if there is no edge y→x (y /= x) exists in
+-- | isTopoFirst x g == true if there is no edge y→x (y /= x) exists in
 -- graph g. Note any node that is not a node in g is a topo-free node
 -- to g. And a node x∈g that has only incoming edge x→x is also
 -- topo-free to g
-isTopoFree :: (Eq a) => a -> Graph a -> Bool
-isTopoFree a Empty = True
-isTopoFree a (Vertex b) = True
-isTopoFree a (Overlay left  right) = isTopoFree a left && isTopoFree a right
-isTopoFree a (Connect left Empty) = isTopoFree a left
-isTopoFree a (Connect left right) = isTopoFree a left && (not $ hasVertex a right)
+isTopoFirst :: (Eq a) => a -> Graph a -> Bool
+isTopoFirst a Empty = True
+isTopoFirst a (Vertex b) = True
+isTopoFirst a (Overlay left  right) = isTopoFirst a left && isTopoFirst a right
+isTopoFirst a (Connect left Empty) = isTopoFirst a left
+isTopoFirst a (Connect left right) = isTopoFirst a left && (not $ hasVertex a right)
 
--- | findTopoFirst g tries to find a node x∈g that is topo-free to g
+-- | isTopoLast x g == true if there is no edge x→y (x /= y) exists in
+-- graph g.
+isTopoLast :: (Eq a) => a -> Graph a -> Bool
+isTopoLast _ Empty = True
+isTopoLast _ (Vertex _) = True
+isTopoLast a (Overlay left right) = isTopoLast a left && isTopoLast a right
+isTopoLast a (Connect Empty right) = isTopoLast a right
+isTopoLast a (Connect left right) = isTopoLast a right && (not $ hasVertex a left)
+
+-- | findTopoFirst g tries to find a node x∈g that is topo-first to g.
 -- If no such node is found, return Nothing, otherwise return Just x.
 findTopoFirst :: (Eq a) => Graph a -> Maybe a
 findTopoFirst g = findTopoFirst' g Empty
 
--- | findTopoFirst' g g' tries to find a node x∈g that is topo-free to
+-- | findTopoLast g tries to find a node x∈g that is topo-last to
+-- g. If no such node is found, return Nothing, otherwise return Just
+-- x
+findTopoLast :: (Eq a) => Graph a -> Maybe a
+findTopoLast g = findTopoLast' g Empty
+
+-- | findTopoFirst' g g' tries to find a node x∈g that is topo-first to
 -- both g and g'. If no such node is found, return Nothing, otherwise
 -- return Just x
 findTopoFirst' :: (Eq a) => Graph a -> Graph a -> Maybe a
 findTopoFirst' Empty _ = Nothing
-findTopoFirst' (Vertex b) g' = if isTopoFree b g' then Just b else Nothing
+findTopoFirst' (Vertex b) g' = if isTopoFirst b g' then Just b else Nothing
 findTopoFirst' (Overlay left right) g' =
   case findTopoFirst' left (Overlay right g') of
     Just a -> Just a
@@ -35,10 +52,23 @@ findTopoFirst' (Connect left right) g' =  findTopoFirst' left newg'
                   -- node even it exists in right, which indicates a
                   -- self-looping edge a→a
                   otherwise -> (Connect g' right)
+-- | findTopoLast' g g' tries to find a node x∈g that is topo-last to
+-- both g and g'.
+findTopoLast' :: (Eq a) => Graph a -> Graph a -> Maybe a
+findTopoLast' Empty _ = Nothing
+findTopoLast' (Vertex b) g' = if isTopoLast b g' then Just b else Nothing
+findTopoLast' (Overlay left right) g' =
+  case findTopoLast' left (Overlay right g') of
+    Just a -> Just a
+    Nothing -> findTopoLast' right (Overlay left g')
+findTopoLast' (Connect left right) g' = findTopoLast' right newg'
+  where newg' = case right of
+                  Vertex a -> g'
+                  otherwise -> (Connect left g')
 
 -- | topoFoldl tries to fold a graph in topological order. When there
--- is a edge x→y in the graph, x is always folded before y. If x and y
--- are not connected by any path, which node is folded first is
+-- is an edge x→y in the graph, x is always folded before y. If x and
+-- y are not connected by any path, which node is folded first is
 -- undetermined. topoFoldl stops when no node can be folded in the
 -- graph and returns the folded value and the sub-graph of unfolded
 -- nodes.
@@ -71,4 +101,18 @@ topoFoldl f b (Vertex a) = ((f b a), Empty)
 topoFoldl f b g =
   case findTopoFirst g of
     Just a -> topoFoldl f (f b a) (removeVertex a g)
+    Nothing -> (b, g)
+
+-- | topoFoldr tries to fold a graph in reversed topological
+-- order. When there is an edge x→y in the graph, y is always folded
+-- before x. If x and y are not connected by any path, which node is
+-- folded first is undetermined. topoFoldr stops when no node can be
+-- folded in the graph and returns the folded value and the sub-graph
+-- of unfolded nodes.
+topoFoldr :: Eq a => (a -> b -> b) -> b -> Graph a -> (b, Graph a)
+topoFoldr f b Empty = (b, Empty)
+topoFoldr f b (Vertex a) = ((f a b), Empty)
+topoFoldr f b g =
+  case findTopoLast g of
+    Just a -> topoFoldr f (f a b) (removeVertex a g)
     Nothing -> (b, g)
--- test/TestTopoGraph.hs
+++ test/TestTopoGraph.hs
@@ -18,31 +18,50 @@ v_loop2_v = overlay (edges [(2, 0), (1, 3)]) loop2
 
 
 
-testIsTopoFree :: Graph Int -> [Bool] -> Test
-testIsTopoFree g goldens
-  = TestCase $ assertEqual "" goldens (map (\n -> isTopoFree n g) nodes)
+testIsTopo :: (Int -> Graph Int -> Bool) -> Graph Int -> [Bool] -> Test
+testIsTopo f g goldens
+  = TestCase $ assertEqual "" goldens (map (\n -> f n g) nodes)
   where nodes = take (1 + vertexCount g) (iterate (+1) 0)
 
-isTopoFreeTests = TestList [
-  testIsTopoFree empty [True],
-  testIsTopoFree oneV [True, True],
-  testIsTopoFree oneE [True, False, True],
-  testIsTopoFree loop [False, True],
-  testIsTopoFree island [True, True, True],
-  testIsTopoFree path2 [True, False, False, True],
-  testIsTopoFree loop2 [False, False, True],
-  testIsTopoFree converge2 [True, False, True, True],
-  testIsTopoFree diverge2 [True, False, False, True],
-  testIsTopoFree v_loop_v [False, True, False, True],
-  testIsTopoFree v_loop2_v [False, False, True, False, True]]
+testIsTopoFirst = testIsTopo isTopoFirst
 
-testFindTopoFirst :: Graph Int -> [Int] -> Test
-testFindTopoFirst g candidates =
+isTopoFirstTests = TestList [
+  testIsTopoFirst empty [True],
+  testIsTopoFirst oneV [True, True],
+  testIsTopoFirst oneE [True, False, True],
+  testIsTopoFirst loop [False, True],
+  testIsTopoFirst island [True, True, True],
+  testIsTopoFirst path2 [True, False, False, True],
+  testIsTopoFirst loop2 [False, False, True],
+  testIsTopoFirst converge2 [True, False, True, True],
+  testIsTopoFirst diverge2 [True, False, False, True],
+  testIsTopoFirst v_loop_v [False, True, False, True],
+  testIsTopoFirst v_loop2_v [False, False, True, False, True]]
+
+testIsTopoLast = testIsTopo isTopoLast
+isTopoLastTests = TestList [
+  testIsTopoLast empty [True],
+  testIsTopoLast oneV [True, True],
+  testIsTopoLast oneE [False, True, True],
+  testIsTopoLast loop [False, True],
+  testIsTopoLast island [True, True, True],
+  testIsTopoLast path2 [False, False, True, True],
+  testIsTopoLast loop2 [False, False, True],
+  testIsTopoLast converge2 [False, True, False, True],
+  testIsTopoLast diverge2 [False, True, True, True],
+  testIsTopoLast v_loop_v [False, False, True, True],
+  testIsTopoLast v_loop2_v [False, False, False, True, True]]
+
+testFindTopo :: (Graph Int -> Maybe Int) -> Graph Int -> [Int] -> Test
+testFindTopo f g candidates =
   TestCase (case result of
               Just a -> assertBool msg (a `elem` candidates)
               Nothing -> assertBool msg (candidates == []))
-  where result = findTopoFirst g
+  where result = f g
         msg = "Found " ++ show result ++ ", but candidates are " ++ show candidates
+
+testFindTopoFirst = testFindTopo findTopoFirst
+testFindTopoLast = testFindTopo findTopoLast
 
 findTopoFirstTests =
   TestList [ testFindTopoFirst empty []
@@ -56,6 +75,19 @@ findTopoFirstTests =
            , testFindTopoFirst diverge2 [0]
            , testFindTopoFirst v_loop_v [1]
            , testFindTopoFirst v_loop2_v [2]]
+
+findTopoLastTests =
+  TestList [ testFindTopoLast empty []
+           , testFindTopoLast oneV [0]
+           , testFindTopoLast oneE [1]
+           , testFindTopoLast loop [0]
+           , testFindTopoLast island [0, 1]
+           , testFindTopoLast path2 [2]
+           , testFindTopoLast loop2 []
+           , testFindTopoLast converge2 [1]
+           , testFindTopoLast diverge2 [1, 2]
+           , testFindTopoLast v_loop_v [2]
+           , testFindTopoLast v_loop2_v [3]]
 
 -- | An edge x→y is in a topo list l when both x and y are in l, and x
 -- appears before y.
@@ -84,9 +116,31 @@ topoFoldlTests = TestList [ testTopoFoldl empty [] []
                           ]
   
 
-allTests = TestList [ TestLabel "isTopoFree" $ isTopoFreeTests
+testTopoFoldr :: Graph Int -> [(Int, Int)] -> [(Int, Int)] -> Test
+testTopoFoldr g topoEdges cyclicEdges =
+  let (topoList, cyclicGraph) = topoFoldr (:) [] g
+  in TestCase (do assertEqual "Duplicated topoList" (nub topoList) topoList
+                  mapM (hasEdge topoList) topoEdges
+                  assertEqual ""  cyclicEdges (edgeList cyclicGraph))
+
+topoFoldrTests = TestList [ testTopoFoldr empty [] []
+                          , testTopoFoldr oneV [(0, 0)] []
+                          , testTopoFoldr oneE [(0, 1)] []
+                          , testTopoFoldr loop [(0, 0)] []
+                          , testTopoFoldr island [(0, 0), (1, 1)] []
+                          , testTopoFoldr path2 [(0, 1), (1, 2)] []
+                          , testTopoFoldr loop2 [] [(0, 1), (1, 0)]
+                          , testTopoFoldr converge2 [(0, 1), (2, 1)] []
+                          , testTopoFoldr diverge2 [(0, 1), (0, 2)] []
+                          , testTopoFoldr v_loop_v [(1, 0), (0, 2)] []
+                          , testTopoFoldr v_loop2_v [(3, 3)] [(0, 1), (1, 0), (2, 0)]]
+
+allTests = TestList [ TestLabel "isTopoFirst" $ isTopoFirstTests
+                    , TestLabel "isTopoLast" $ isTopoLastTests
                     , TestLabel "findTopoFirst" $ findTopoFirstTests
+                    , TestLabel "findTopoLast" $ findTopoLastTests
                     , TestLabel "topoFoldlTests" $ topoFoldlTests
+                    , TestLabel "topoFoldrTests" $ topoFoldrTests
                     ]
 
 
